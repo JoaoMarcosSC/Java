0f18abc55c78f7e7744cf5ce866e547d361f5f7a
João Marcos <47647644+JoaoMarcosSC@users.noreply.github.com>
2022-12-30 13:57:18 -0300

    Atividade Acadêmica
    Desenvolvimento de ferramenta para venda e controle de estoque de garrafas de agua.

---
+++ JavaOrientadoAObjetos/VendaDeGarrafas/Produto.java
@@ -0,0 +1,20 @@
+public class Produto {
+    private String nome;
+    private float  valor;
+
+    public void setValor(float valor){
+        this.valor = valor;
+    }
+    
+    public float getValor(){
+        return this.valor;
+    }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public String getNome(){
+        return this.nome;
+    }
+}
