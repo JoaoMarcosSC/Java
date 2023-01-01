b2bbdbb40d38ea59a1fbbdb5a0b6d45e1761f274
João Marcos <47647644+JoaoMarcosSC@users.noreply.github.com>
2022-12-31 23:00:29 -0300

    Criação de Métodos de Saque e Transferência

--- JavaOrientadoAObjetos/ConhecimentosBasicos/ProjetoByteBank/src/TestaMetodo.java
+++ JavaOrientadoAObjetos/ConhecimentosBasicos/ProjetoByteBank/src/TestaMetodo.java
@@ -1,10 +1,29 @@
 public class TestaMetodo {
     public static void main(String[] args) {
-        Conta conta = new Conta();
-        conta.saldo = 50;
+        Conta contaDoPaulo = new Conta();
+        contaDoPaulo.saldo = 50;
+        contaDoPaulo.titular = "Paulo Silva";
 
-        conta.deposita(50);
-        System.out.println(conta.saldo);
+        contaDoPaulo.deposita(50);
+        boolean conseguiuRetirar = contaDoPaulo.saca(20);
+        System.out.println(conseguiuRetirar);
+        System.out.println(contaDoPaulo.saldo);
+
+        Conta contaDaMarcela = new Conta();
+        contaDaMarcela.saldo = 100;
+        contaDaMarcela.titular = "Marcela Souza";
+
+        contaDaMarcela.deposita(50);
+        System.out.println(contaDaMarcela.saldo);
+
+        if (contaDaMarcela.tranfere(50, contaDoPaulo)){
+            System.out.println("Tranferencia realizada com sucesso");
+        }else{
+            System.out.println("Erro na Tranferencia");
+        }
+
+        System.out.println(contaDoPaulo.saldo);
+        System.out.println(contaDaMarcela.saldo);
 
     }
 }
