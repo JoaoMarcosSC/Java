eaaa76f82f9eb2c46b210533b4409dcb6e6a51be
João Marcos <47647644+JoaoMarcosSC@users.noreply.github.com>
2025-12-19 20:22:18 -0300

    14. Persistindo produtos no banco de dados

--- Spring Boot/produtosapi/src/main/java/io/github/cursodsousa/produtosapi/controller/ProdutoController.java
+++ Spring Boot/produtosapi/src/main/java/io/github/cursodsousa/produtosapi/controller/ProdutoController.java
@@ -1,10 +1,13 @@
 package io.github.cursodsousa.produtosapi.controller;
 
 import io.github.cursodsousa.produtosapi.model.Produto;
+import io.github.cursodsousa.produtosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("produtos")
