81f388d3b0e7d0997829f6588616dec02c6d2f9c
João Marcos <47647644+JoaoMarcosSC@users.noreply.github.com>
2025-12-19 20:22:35 -0300

    14. Persistindo produtos no banco de dados

--- Spring Boot/produtosapi/src/main/java/io/github/cursodsousa/produtosapi/controller/ProdutoController.java
+++ Spring Boot/produtosapi/src/main/java/io/github/cursodsousa/produtosapi/controller/ProdutoController.java
@@ -14,9 +14,18 @@ import java.util.UUID;
 
 public class ProdutoController {
 
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
     @PostMapping
     public Produto salvar(@RequestBody Produto produto){
-        System.out.println("Produto recebido" + produto);
+        System.out.println("Produto recebido " + produto);
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+        produtoRepository.save(produto);
         return produto;
     }
 }
