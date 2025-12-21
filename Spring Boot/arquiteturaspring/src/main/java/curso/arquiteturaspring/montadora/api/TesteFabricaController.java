a1d480e033387e41d8c611f1fd3f7bd4c36c2c6e
João Marcos <47647644+JoaoMarcosSC@users.noreply.github.com>
2025-12-21 16:04:17 -0300

    27. Utilizando Qualifiers

--- Spring Boot/arquiteturaspring/src/main/java/curso/arquiteturaspring/montadora/api/TesteFabricaController.java
+++ Spring Boot/arquiteturaspring/src/main/java/curso/arquiteturaspring/montadora/api/TesteFabricaController.java
@@ -5,15 +5,21 @@ import curso.arquiteturaspring.montadora.Chave;
 import curso.arquiteturaspring.montadora.HondaHRV;
 import curso.arquiteturaspring.montadora.Motor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("carros")
 public class TesteFabricaController {
 
     @Autowired
+    @Qualifier("motorTurbo")
     private Motor motor;
 
+    @PostMapping
     public CarroStatus ligarCarro(@RequestBody Chave chave){
         var carro = new HondaHRV(motor);
         return carro.darIgnicao(chave);
--- Spring Boot/arquiteturaspring/src/main/java/curso/arquiteturaspring/montadora/configuration/MontadoraConfiguration.java
+++ Spring Boot/arquiteturaspring/src/main/java/curso/arquiteturaspring/montadora/configuration/MontadoraConfiguration.java
@@ -8,14 +8,36 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MontadoraConfiguration {
 
-    @Bean
-    public Motor motor(){
+    @Bean(name = "motorAspirado")
+    public Motor motorAspirado(){
         var motor = new Motor();
         motor.setCavalos(120);
         motor.setCilindros(4);
         motor.setModelo("XPTO-0");
         motor.setLitragem(2.0);
         motor.setTipo(TipoMotor.ASPIRADO);
+        return motor;
+    }
+
+    @Bean(name = "motorEletrico")
+    public Motor motorEletrico(){
+        var motor = new Motor();
+        motor.setCavalos(80);
+        motor.setCilindros(3);
+        motor.setModelo("XPTO-0");
+        motor.setLitragem(1.0);
+        motor.setTipo(TipoMotor.ELETRICO);
+        return motor;
+    }
+
+    @Bean(name = "motorTurbo")
+    public Motor motorTurbo(){
+        var motor = new Motor();
+        motor.setCavalos(180);
+        motor.setCilindros(4);
+        motor.setModelo("XPTO-0");
+        motor.setLitragem(1.5);
+        motor.setTipo(TipoMotor.TURBO);
         return motor;
     }
 }
