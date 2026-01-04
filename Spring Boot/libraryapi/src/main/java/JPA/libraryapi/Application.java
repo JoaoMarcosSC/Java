343ed8f2882e51365834181c75e0a38df165db81
João Marcos <47647644+JoaoMarcosSC@users.noreply.github.com>
2026-01-04 15:55:19 -0300

    58. Aplicando testes no repository

--- Spring Boot/libraryapi/src/main/java/JPA/libraryapi/Application.java
+++ Spring Boot/libraryapi/src/main/java/JPA/libraryapi/Application.java
@@ -1,7 +1,5 @@
 package JPA.libraryapi;
 
-import JPA.libraryapi.model.Autor;
-import JPA.libraryapi.repository.AutorRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,20 +9,7 @@ import java.time.LocalDate;
 public class Application {
 
 	public static void main(String[] args) {
-		var context = SpringApplication.run(Application.class, args);
-		AutorRepository repository = context.getBean(AutorRepository.class);
-
-		exemploSalvarRegistro(repository);
-	}
-
-	public static void exemploSalvarRegistro(AutorRepository autorRepository){
-		Autor autor = new Autor();
-		autor.setNome("José");
-		autor.setNacionalidade("Brasileira");
-		autor.setDataNascimento(LocalDate.of(1950, 1, 31));
-
-		var autorSalvo = autorRepository.save(autor);
-		System.out.println("Autor Salvo " + autorSalvo);
+		SpringApplication.run(Application.class, args);
 
 	}
 
