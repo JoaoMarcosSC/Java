9597ff350319b3b7a107f014c2f9306250011b56
João Marcos <47647644+JoaoMarcosSC@users.noreply.github.com>
2026-01-04 15:23:57 -0300

    57. Executando uma operação de persistencia com Repositorio

---
+++ Spring Boot/libraryapi/src/main/java/JPA/libraryapi/repository/AutorRepository.java
@@ -0,0 +1,9 @@
+package JPA.libraryapi.repository;
+
+import JPA.libraryapi.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AutorRepository extends JpaRepository<Autor, UUID> {
+}
