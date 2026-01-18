84b18dd737d0690499fff3ec549676b190c4ebf4
João Marcos <47647644+JoaoMarcosSC@users.noreply.github.com>
2026-01-18 16:44:44 -0300

    Exercícios Estrutura Sequencial

---
+++ JavaOrientadoAObjetos/4 Estrutura Sequencial/EstruturaSequencial/src/exercicio2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class exercicio2 {
+
+    public static void main(String[] args) {
+
+        float x1;
+        float pi = 3.14159F;
+        float z;
+
+        Scanner sc = new Scanner(System.in);
+        x1 = sc.nextFloat();
+
+        z = (float) (pi * Math.pow(x1, 2));
+
+        System.out.println("Area = " + z);
+
+        sc.close();
+    }
+}
+
---
+++ JavaOrientadoAObjetos/4 Estrutura Sequencial/EstruturaSequencial/src/exercicio3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class exercicio3 {
+
+    public static void main(String[] args) {
+
+        int A;
+        int B;
+        int C;
+        int D;
+
+        int x;
+
+        Scanner sc = new Scanner(System.in);
+        A = sc.nextInt();
+        B = sc.nextInt();
+        C = sc.nextInt();
+        D = sc.nextInt();
+
+        x = A * B - C * D;
+
+        System.out.println("Diferença = " + x);
+
+        sc.close();
+    }
+}
