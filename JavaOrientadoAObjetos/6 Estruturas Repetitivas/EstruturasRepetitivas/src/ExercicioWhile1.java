3cec343da506e47fea18f6216a29a58755c7a59d
João Marcos <47647644+JoaoMarcosSC@users.noreply.github.com>
2026-01-19 10:01:01 -0300

    Exercícios sobre estruturas repetitivas

---
+++ JavaOrientadoAObjetos/6 Estruturas Repetitivas/EstruturasRepetitivas/src/ExercicioWhile1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ExercicioWhile1 {
+    public static void main(String[] args) {
+
+        String x;
+        String s = "";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Crie sua senha");
+        x = sc.next();
+
+        while (!x.equals(s)) {
+            System.out.println("Digite sua senha");
+            s = sc.next();
+
+            if (!x.equals(s)) {
+                System.out.println("Senha incorreta");
+            }
+        };
+
+        System.out.println("Senha Correta");
+
+    }
+}
